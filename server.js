cf71bb65d848d80c92fc72b49ee6e1ce097c35f0
Pedro Evangelista <pedroevangelista@outlook.com>
2020-03-29 19:19:56 -0300

    Back-end node.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
+
+
+//iniciando aplicação
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+}); 
+
+requireDir('./src/models');
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
